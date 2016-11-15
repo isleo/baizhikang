3ee994c8c260790b8aa506e294ddc87b72d5164e
chenYx <1014732534@qq.com>
2016-11-15 20:31:17 +0800

    test

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -237,7 +237,7 @@ class UserController extends BaseController
             }
         } catch (QueryException $e) {
             $retval['status'] = -4;
-            $retval['msg'] = $e->formatMessage();
+            $retval['msg'] = $e->message;
             return response()->json($retval);
         }
     }
