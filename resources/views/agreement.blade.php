046d92470aa6e61062b5d94769cccc2e15b48602
leo <zengyingle@aipai.com>
2016-11-25 17:52:04 +0800

    用户协议

--- resources/views/agreement.blade.php
+++ resources/views/agreement.blade.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>拜托啦——登录</title>
+<title>百智康协议</title>
 <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=no">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
-<div>sdfsfd</div>
+<h1>百智康协议</h1>
 </body>
 </html>
